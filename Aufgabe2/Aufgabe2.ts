4dd4937e3b660b346dc08ebffb94958fdc54df4b
stueberd <dimitrios.stueber@hs-furtwangen.de>
2019-04-07 17:42:58 +0200

    verzweifelte Versuche

--- Aufgabe2/Aufgabe2.ts
+++ Aufgabe2/Aufgabe2.ts
@@ -14,6 +14,9 @@ function init() {
     let Deck: string[] = ["krz7", "krz8", "krz9", "krzb", "krzd", "krzk", "krz1", "krza", "pi7", "pi8", "pi9", "pib", "pid", "pik", "pi1", "pia", "her7", "her8", "her9", "herb", "herd", "herk", "her1", "hera", "kar7", "kar8", "kar9", "karb", "kard", "kark", "kar1", "kara"];
     randomCards(0, Deck);
 }
+let zufallsNr: number = math.Random* Deck.length;
+i:number=0;
+if i < Handkarten[];
 
 
 document.addEventListener('DOMContentLoaded', init);
