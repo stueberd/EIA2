4200e2e5a46f7ba64672ee15fc8af8a0c0173a92
stueberd <dimitrios.stueber@hs-furtwangen.de>
2019-04-07 17:26:35 +0200

    upload

---
+++ Aufgabe2/Aufgabe2.js
@@ -0,0 +1,14 @@
+/* Aufgabe 2 - Mau-Mau
+Name: Dimitrios Stüber
+Matrikel: 257744
+Datum: 07.04.2019
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert. */
+function randomCards(_numberOfCards, _HandkartenArray) {
+    var cardsSelected = [];
+}
+function init() {
+    let allCards = ["krz7", "krz8", "krz9", "krzb", "krzd", "krzk", "krz1", "krza", "pi7", "pi8", "pi9", "pib", "pid", "pik", "pi1", "pia", "her7", "her8", "her9", "herb", "herd", "herk", "her1", "hera", "kar7", "kar8", "kar9", "karb", "kard", "kark", "kar1", "kara"];
+    randomCards(0, allCards);
+}
+document.addEventListener('DOMContentLoaded', init);
+//# sourceMappingURL=Aufgabe2.js.map
---
+++ Aufgabe2/Aufgabe2.ts
@@ -0,0 +1,19 @@
+/* Aufgabe 2 - Mau-Mau
+Name: Dimitrios Stüber
+Matrikel: 257744
+Datum: 07.04.2019
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert. */
+
+function randomCards(_numberOfCards: number, _HandkartenArray: string[]) {
+    let cardsSelected: string[] = [];
+
+}
+
+
+function init() {
+    let Deck: string[] = ["krz7", "krz8", "krz9", "krzb", "krzd", "krzk", "krz1", "krza", "pi7", "pi8", "pi9", "pib", "pid", "pik", "pi1", "pia", "her7", "her8", "her9", "herb", "herd", "herk", "her1", "hera", "kar7", "kar8", "kar9", "karb", "kard", "kark", "kar1", "kara"];
+    randomCards(0, Deck);
+}
+
+
+document.addEventListener('DOMContentLoaded', init);
