2963b9ae4e73c2fdb9d5c29cc7d8e0ebb3caa11a
stueberd <dimitrios.stueber@hs-furtwangen.de>
2020-05-02 23:30:57 +0200

    upload

---
+++ Au1/Aufg2.ts
@@ -0,0 +1,16 @@
+namespace SequenceMemory {
+
+    window.addEventListener("load", init);
+
+    function init(): void {
+        let fieldsets: HTMLCollectionOf<HTMLFieldSetElement> = document.getElementsByTagName("fieldset");
+
+        for (let i: number = 0; i < fieldsets.length; i++) {
+            let fieldset: HTMLFieldSetElement = fieldsets[i];
+           }
+    }
+ 
+    
+
+
+} 
